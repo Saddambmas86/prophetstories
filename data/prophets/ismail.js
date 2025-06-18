daeb2501b9027764fcf6119724f3df2b6e1f0f9c
Saddambmas86 <Saddambmas86@gmail.com>
2025-06-18 21:17:20 +0530

    Update ismail.js

--- data/prophets/ismail.js
+++ data/prophets/ismail.js
@@ -1,7 +1,7 @@
 export const ismail = {
     name: "Prophet Ismail (AS)",
         category: "pre-islamic",
-        image: "images/ismail_as.png",
+        image: "images/Ismail_as.png",
         description: "The son of Ibrahim and builder of the Kaaba",
         biography: "Prophet Ismail (AS) was the son of Ibrahim and Hajar. He and his mother were left in the desert of Mecca, where the Zamzam well sprang forth. He helped his father build the Kaaba and was tested with the command of sacrifice. He became a prophet and preached to the people of Arabia.",
         timeline: "Eighth Prophet",
