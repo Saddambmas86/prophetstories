882268b3584dd73c2b5d750116c09c0be08db04d
Saddambmas86 <Saddambmas86@gmail.com>
2025-06-18 21:18:48 +0530

    Update shuaib.js

--- data/prophets/shuaib.js
+++ data/prophets/shuaib.js
@@ -1,7 +1,7 @@
 export const shuaib = {
     name: "Prophet Shu'aib (AS)",
         category: "pre-islamic",
-        image: "images/shuaib_as.png",
+        image: "images/Shuaib_as.png",
         description: "The prophet of the people of Madyan",
         biography: "Prophet Shu'aib (AS) was sent to the people of Madyan, who were known for their dishonesty in business and worship of a sacred tree. He warned them of Allah's punishment, but they mocked him. They were destroyed by an earthquake and a cry from heaven, while Shu'aib and the believers were saved.",
         timeline: "Thirteenth Prophet",
