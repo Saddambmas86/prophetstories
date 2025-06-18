ff04bf8b2cdaf6bd5355e8d750f1c9a1cb0f134f
Saddambmas86 <Saddambmas86@gmail.com>
2025-06-18 21:17:02 +0530

    Update ibrahim.js

--- data/prophets/ibrahim.js
+++ data/prophets/ibrahim.js
@@ -1,7 +1,7 @@
 export const ibrahim = {
     name: "Prophet Ibrahim (AS)",
         category: "pre-islamic",
-        image: "images/ibrahim_as.png",
+        image: "images/Ibrahim_as.png",
         description: "The friend of Allah and father of prophets",
         biography: "Prophet Ibrahim (AS) is known as the friend of Allah and the father of prophets. He rejected idol worship and preached monotheism. He was thrown into a fire but was saved by Allah's command. He built the Kaaba with his son Ismail and was tested with the command to sacrifice his son. He is considered the patriarch of monotheism.",
         timeline: "Sixth Prophet",
