cedd42cd896f59682e73f5a6d8bdb3a033af632d
Saddambmas86 <Saddambmas86@gmail.com>
2025-06-18 21:19:30 +0530

    Update alyasa.js

--- data/prophets/alyasa.js
+++ data/prophets/alyasa.js
@@ -1,7 +1,7 @@
 export const alyasa = {
     name: "Prophet Al-Yasa (AS)",
         category: "pre-islamic",
-        image: "images/Al_Yasa_as.png",
+        image: "images/Al_yasa_as.png",
         description: "The successor of Ilyas",
         biography: "Prophet Al-Yasa (AS) was the successor of Ilyas and continued his mission. He performed miracles and called people to worship Allah alone. He was known for his piety and dedication to spreading the message of monotheism. He continued the struggle against idol worship.",
         timeline: "Nineteenth Prophet",
