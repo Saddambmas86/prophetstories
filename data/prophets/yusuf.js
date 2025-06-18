5cd1dbc50dcb683a385efa8aa55015107df25f4e
Saddambmas86 <Saddambmas86@gmail.com>
2025-06-18 21:18:23 +0530

    Update yusuf.js

--- data/prophets/yusuf.js
+++ data/prophets/yusuf.js
@@ -1,7 +1,7 @@
 export const yusuf = {
      name: "Prophet Yusuf (AS)",
         category: "pre-islamic",
-        image: "images/yusuf_as.png",
+        image: "images/Yusuf_as.png",
         description: "The prophet with the beautiful story",
         biography: "Prophet Yusuf (AS) was the son of Yaqub who was thrown into a well by his brothers. He was sold into slavery in Egypt, where he rose to prominence due to his ability to interpret dreams. He was falsely imprisoned but later became a minister. He forgave his brothers and was reunited with his family.",
         timeline: "Eleventh Prophet",
