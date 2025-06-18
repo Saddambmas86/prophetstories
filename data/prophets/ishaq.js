6c2d1faa88490c3ad133516fd190fe54a2a5d74b
Saddambmas86 <Saddambmas86@gmail.com>
2025-06-18 21:17:44 +0530

    Update ishaq.js

--- data/prophets/ishaq.js
+++ data/prophets/ishaq.js
@@ -1,7 +1,7 @@
 export const ishaq = {
      name: "Prophet Ishaq (AS)",
         category: "pre-islamic",
-        image: "images/ishaq_as.png",
+        image: "images/Ishaq_as.png",
         description: "The son of Ibrahim and father of Yaqub",
         biography: "Prophet Ishaq (AS) was the son of Ibrahim and Sarah, born when they were very old. He was a prophet and the father of Yaqub. He continued his father's mission of preaching monotheism and was known for his wisdom and piety. The Israelites are among his descendants.",
         timeline: "Ninth Prophet",
