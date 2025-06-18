a09c0bdbb28958b581a47b2d9684f8f251c1a68a
Saddambmas86 <Saddambmas86@gmail.com>
2025-06-18 21:18:04 +0530

    Update yaqub.js

--- data/prophets/yaqub.js
+++ data/prophets/yaqub.js
@@ -1,7 +1,7 @@
 export const yaqub = {
     name: "Prophet Yaqub (AS)",
         category: "pre-islamic",
-        image: "images/yaqub_as.png",
+        image: "images/Yaqub_as.png",
         description: "The father of the twelve tribes",
         biography: "Prophet Yaqub (AS), also known as Israel, was the son of Ishaq and the father of twelve sons who became the twelve tribes of Israel. He was known for his patience and wisdom. He was tested with the loss of his son Yusuf but was eventually reunited with him. He continued the mission of his forefathers.",
         timeline: "Tenth Prophet",
